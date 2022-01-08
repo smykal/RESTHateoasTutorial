74597c42e69b299e1a4f318d7b761f002cfc294a
Mateusz <smykal@engineer.com>
2022-01-08 13:02:46 +0100

    start project with Spring Initializr with Hateoas and Lombok dependencies

---
+++ src/main/java/com/example/RESTHateoasTutorial/RestHateoasTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.RESTHateoasTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHateoasTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHateoasTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RESTHateoasTutorial/RestHateoasTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RESTHateoasTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestHateoasTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
